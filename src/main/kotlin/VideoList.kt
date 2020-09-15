a37393d9b437dc5adf1789aa3ca535cce21fe99f
Jean-Michel Fayard <jmfayard@gmail.com>
2020-09-15 12:10:36 +0200

    Working together: Composing components
    https://play.kotlinlang.org/hands-on/Building%20Web%20Applications%20with%20React%20and%20Kotlin%20JS/05_Working_Together_Composing_Components

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -5,24 +5,26 @@ import react.dom.*
 
 external interface VideoListProps: RProps {
     var videos: List<Video>
-}
-external interface VideoListState: RState {
     var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
 }
 
-class VideoList: RComponent<VideoListProps, VideoListState>() {
+fun RBuilder.videoList(handler: VideoListProps.() -> Unit): ReactElement {
+    return child(VideoList::class) {
+        this.attrs(handler)
+    }
+}
+class VideoList: RComponent<VideoListProps, RState>() {
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
                 attrs {
                     onClickFunction = {
-                        setState {
-                            selectedVideo = video
-                        }
+                        props.onSelectVideo(video)
                     }
                 }
-                if (video == state.selectedVideo) {
+                if (video == props.selectedVideo) {
                     +"▶ "
                 }
                 +"${video.speaker}: ${video.title}"
